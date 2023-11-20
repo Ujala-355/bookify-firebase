e22a3dd9e3c94d43e1e592b6f31fdddba757f377
Ujala <ujala21@navgurukul.com>
2023-11-20 19:54:29 +0530

    create list and using CRUD storing data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 // pages
 import RegisterPage from "./pages/Register";
 import LoginPage from "./pages/Login";
+import ListingPage from "./pages/List";
+
 import {Routes, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+// Components
+import MyNavbar from "./component/Navbar";
+
 
 import './App.css';
 
 function App() {
   return (
-
-      <Routes>
-          <Route path="/" element={<h1>Home</h1>}/>
-          <Route path="/login" element={<LoginPage/>}/>
-          <Route path="/register" element={<RegisterPage/>}/>
-      </Routes>
+      <>
+          <MyNavbar/>
+          <Routes>
+              <Route path="/" element={<h1>Home</h1>}/>
+              <Route path="/login" element={<LoginPage/>}/>
+              <Route path="/register" element={<RegisterPage/>}/>
+              <Route path="/book/list" element={<ListingPage/>}/>
+          </Routes>
+      </>
   );
 }
 
--- src/context/Firebase.js
+++ src/context/Firebase.js
@@ -8,6 +8,9 @@ import {
     signInWithPopup,
     onAuthStateChanged,
 } from "firebase/auth";
+import {getFirestore,collection,addDoc} from "firebase/firestore";
+import {getStorage,ref,uploadBytes} from "firebase/storage";
+
 const FirebaseContext=createContext(null);
 const firebaseConfig = {
     apiKey: "AIzaSyBkI_z5YUUz7P-2lKBdEEEXjY8ROBJNFL8",
@@ -21,15 +24,22 @@ export const useFirebase=()=>useContext(FirebaseContext);
 
 const firebaseApp=initializeApp(firebaseConfig);
 const firebaseAuth=getAuth(firebaseApp);
+const firestore=getFirestore(firebaseApp);
+const storage=getStorage(firebaseApp);
+
 const googleProvider=new GoogleAuthProvider()
 
 export const FirebaseProvider=(props)=>{
     const [user, setUser]=useState(null);
     useEffect(()=>{
         onAuthStateChanged(firebaseAuth, user=>{
-            if(user) setUser(user);
-            else setUser(null);
+            if(user){ 
+                setUser(user);
+            }
+            else{ 
+                setUser(null);
             console.log("User",user)
+            }
         })
     },[]);
     const signupUserWithEmailAndPassword=(email,password)=>
@@ -39,6 +49,22 @@ export const FirebaseProvider=(props)=>{
         signInWithEmailAndPassword(firebaseAuth,email,password);
     
     const signinWithGoogle=()=>signInWithPopup(firebaseAuth,googleProvider);
+
+    const handleCreateNewListing=async(name,isbn,price,cover)=>{
+        const imageRef=ref(storage, `uploads/images/${Date.now()}-${cover.name}`);
+        const uploadResult= await uploadBytes(imageRef,cover);
+        return await addDoc(collection(firestore,'books'),{
+            name,
+            isbn,
+            price,
+            imageURL:uploadResult.ref.fullPath ,
+            userID:user.uid,
+            userEmail:user.email,
+            displayName:user.displayName,
+            photoURL:user.photoURL
+
+        })
+    }
     const isLoggedIn=user ? true:false;
     return(
         <>
@@ -46,6 +72,7 @@ export const FirebaseProvider=(props)=>{
                 signinWithGoogle,
                 signupUserWithEmailAndPassword, 
                 singinUserWithEmailAndPass,
+                handleCreateNewListing,
                 isLoggedIn,
                 }}>
                 {props.children}
